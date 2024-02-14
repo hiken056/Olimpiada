808b3da8550f381847e2988f47973b1367a32cae
hiken056 <stefanrusu60@gmail.com>
2024-02-14 09:05:01 +0200

    dinamic2

---
+++ dinamica/K-based numbers (timus)/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, K;
+int dp[20][20];
+
+int main () {
+    cin >> N >> K;
+    dp[0][0] = 1;
+    for (int i = 1; i <= N; i ++ ){
+        for ( int j = 0; j < K; j ++ ) {
+            if ( j == 0 ) {
+                for ( int k =  1; k < K; ++ k) {
+                    dp[i][j] += dp[i-1][k];
+                }
+            }
+            else {
+                for ( int k = 0; k < K; ++ k ) {
+                    dp[i][j] += dp[i-1][k];
+                }
+             }
+        }
+    }
+    int S = 0;
+    for ( int i = 0; i < K; i ++ ) {
+        S += dp[N][i];
+    }
+    cout << S;
+    return 0;
+}
