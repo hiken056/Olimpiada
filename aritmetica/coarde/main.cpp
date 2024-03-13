7dbc616a0c8372e8a5f41f8a835b795f62f4e366
hiken056 <stefanrusu60@gmail.com>
2024-03-13 09:12:56 +0200

    coarde (nr catalan)

---
+++ aritmetica/coarde/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin ("coarde.in");
+ofstream fout ("coarde.out");
+
+const int MODULO = 9999991;
+
+int N;
+
+int log_pow(int n, int p) {
+    if ( p == 0 ) return 1;
+    int n2 = log_pow(n, p/2);
+    if ( p % 2 == 0 ) {
+        return (1ll * n2 * n2) % MODULO;
+    }
+    else {
+        return ((((1ll * n2 * n2) % MODULO) * n) % MODULO);
+    }
+}
+
+int catalan(int n) {
+    long long n2fac = 1;
+    long long nfac = 1;
+    long long n_1fac = 1;
+    for ( int i = 1; i <= n*2 ; ++ i ) {
+        n2fac = ( 1ll * n2fac * i ) % MODULO;
+        if ( i == n ) {
+            nfac = n2fac;
+        }
+        if ( i == n + 1) {
+            n_1fac = n2fac; 
+        }
+    }
+    nfac = log_pow(nfac, MODULO - 2);
+    n_1fac = log_pow(n_1fac, MODULO - 2);
+    return ((((1ll * n2fac * nfac) % MODULO ) * n_1fac ) % MODULO);
+}
+
+int main () {
+    fin >> N;
+    int cnt = 1;
+    while ( N != 0 ) {
+        fout << cnt << ":" << catalan(N) << '\n';
+        cnt ++;
+        fin >> N;
+    }
+    return 0;
+}
