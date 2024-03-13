4a02c67c8c48478b6a95528beba078779801df3e
hiken056 <stefanrusu60@gmail.com>
2024-03-13 10:46:22 +0200

    tastatura2

--- greedy/tastatura2/main.cpp
+++ greedy/tastatura2/main.cpp
@@ -2,16 +2,12 @@
 
 using namespace std;
 
-const int MAXN = 20000;
-
 ifstream fin ( "tastatura2.in");
 ofstream fout ("tastatura2.out");
 
 string s, t;
 
 int vfs[256], vft[256];
-
-int sol;
 
 int solve(string s, string t, int sol = 0) {
     for ( const char& i : s ) {
@@ -20,7 +16,7 @@ int solve(string s, string t, int sol = 0) {
     for ( const char& i : t ) {
         vfs[i] ++;
     }
-    for ( int i = 1; i <= 256; ++ i ) {
+    for ( int i = 0; i < 256; ++ i ) {
         if ( vfs[i] != vft[i] ) sol++;
     }
     return sol;
@@ -30,10 +26,13 @@ int main () {
     getline(fin, s);
     getline(fin, t);
     while ( s != "*" ) {
-        solve(s, t);
-        fout << sol << '\n';
+        fout << solve(s, t) << '\n';
         getline(fin, s);
         getline(fin, t);
+        for ( int i = 0; i < 256; ++ i) {
+            vfs[i] = 0;
+            vft[i] = 0;
+        }
     }
     return 0;
 }
