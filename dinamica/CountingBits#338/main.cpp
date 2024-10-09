79e551834fb120e4b4a02e028c850d92413e93a0
hiken056 <stefanrusu60@gmail.com>
2024-10-09 19:48:51 +0300

    Counting Bits rezolvat

--- dinamica/CountingBits#338/main.cpp
+++ dinamica/CountingBits#338/main.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+ifstream fin("date.in");
+ofstream fout ("date.out");
 int main() {
     int n, bit = 0, m = 0;
     cin >> n;
+    cout << "merge\n";
     int countBits[100005];
+    countBits[0] = 0;
+    countBits[1] = 1;
+    //ideea este ca daca nr este par ==> nr de biti o sa fie nr de biti la n/2
+    //daca nr este impar ==> nr de biti o sa fie nr de biti la n/2 + 1
+    for ( int i = 2; i <= n; ++ i ) {
+        if ( i % 2  == 0 ) countBits[i] = countBits[i/2];
+        else countBits[i] = countBits[i/2] + 1;
+    }
     for ( int i = 0; i <= n; ++ i ) {
-        if ( bit <= m ) {
-            countBits[i] = bit;
-            bit ++;
-        }
-        else {
-            countBits[i] = bit;
-            bit = 1;
-            m ++;
-        }
         cout << countBits[i] << " ";
     }
     return 0;
