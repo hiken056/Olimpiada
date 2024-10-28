1a9bcd66b8994dbbad6afb6e09880cc643442040
hiken056 <stefanrusu60@gmail.com>
2024-10-28 19:55:13 +0200

    bellman-ford

--- grafuri/Bellman-Ford/main.cpp
+++ grafuri/Bellman-Ford/main.cpp
@@ -23,12 +23,38 @@ int main () {
         in >> x >> y >> c;
         G[x].push_back({y, c});
     }  
+    for ( int i = 1; i <= N; ++ i ) {
+        dist[i] = oo;
+    }
     dist[1] = 0;
     Q.push(1);
     inQ[1] = 1;
+    
+    while ( ! Q.empty() && ok == 1 ) {
+        int nod = Q.front();
+        Q.pop();
+        inQ[nod] = 0;
+        viz[nod] ++;
+        if ( viz[nod] >= N ) {
+            ok = 0;
+            break;
+        }
+        for ( auto vecin : G[nod] ) {
+            if ( dist[vecin.first] > dist[nod] + vecin.second )  {
+                dist[vecin.first] = dist[nod] + vecin.second;
+                if ( inQ[vecin.first] == 0 ) {
+                    Q.push(vecin.first);
+                    inQ[vecin.first] = 1;
+                }
+            }
+        }
+    }
 
-    while ( ! Q.empty( ) ) {
-        //poate merge 
+    if ( ok == 0 ) out << "Ciclu negativ!";
+    else {
+        for ( int i = 2; i <= N; ++ i ) {
+            out << dist[i] << " ";
+        }
     }
     return 0;
 }
