a6c3fc933a03f7cd56993fa0f6b2e3234cd4b339
hiken056 <stefanrusu60@gmail.com>
2024-11-17 23:32:32 +0200

    ce misto e nvim

--- incercare.cpp
+++ incercare.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    cout << n;
+bool prim(int n) {
+  int d = 2;
+  if (n < 2)
     return 0;
+  if (n == 2)
+    return 1;
+  if (n % 2 == 0)
+    return 0;
+  for (int i = 3; i * i <= n; i += 2) {
+    if (n % i == 0)
+      return 0;
+  }
+  return 1;
+}
 
+int main() {
+  int n;
+  cin >> n;
+  cout << prim(n) << '\n';
+  return 0;
 }
