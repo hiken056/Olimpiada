5b29a5418ed1f0913e559318fac292dc83842ded
hiken056 <stefanrusu60@gmail.com>
2025-03-13 16:44:38 +0200

    triang

--- geometrie/triang/main.cpp
+++ geometrie/triang/main.cpp
@@ -5,33 +5,45 @@ using namespace std;
 ifstream fin ("triang.in");
 ofstream fout ("triang.out");
 
+const double dif = 1e-3;
+
 int N, rez;
 struct punct {
     double x;
     double y;
+    //prima data compar dupa x, iar daca dif este neglijabila compar dupa y
+    bool operator < (const punct& a) const{
+        if ( abs(x - a.x) > dif ) 
+            return x < a.x;
+        if ( abs(y - a.y) > dif ) 
+            return y > a.y;
+        return 0;
+    }
 } puncte[1505];
+set <punct> s;
 
-double distance ( punct a, punct b) {
-    return sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y));
+punct rotatie ( punct a, punct b ) {
+    //sin si cos de 60
+    double cos = 1.0 / 2;
+    double sin = sqrt(3) / 2;
+    punct c;
+    //rotatia unui punct fata de alt punct
+    c.x = a.x + ( b.x - a.x ) * cos - ( b.y - a.y) * sin;
+    c.y = a.y + ( b.x - a.x ) * sin + ( b.y - a.y) * cos;
+    return c;
 }
 
 int main() {
     fin >> N;
     for ( int i = 1; i <= N; ++ i ) {
-        double x, y;
-        fin >> x >> y;
-        puncte[i].x = x;
-        puncte[i].y = y;
-    }
-    for ( int i = 1; i < N -1; ++ i ) {
-        for ( int j = 2; j < N; ++ j ) {
-            double d1 = distance(puncte[i], puncte[j]);
-            for ( int k = 3; k <= N; ++ k ) {
-                double d2 = distance(puncte[i], puncte[k]);
-                double d3 = distance(puncte[j], puncte[k]);
-                if ( d1 == d2 && d1 == d3 && d2 == d3 ) rez ++;
+        fin >> puncte[i].x >> puncte[i].y;
+        //verificam daca gasim punctul pe care il rotim
+        for ( int j = 1; j  < N; ++ j ) {
+            if ( s.find(rotatie(puncte[i], puncte[j])) != s.end() ) {
+                rez ++;
             }
         }
+        s.insert(puncte[i]);
     }
     fout << rez;
 }
